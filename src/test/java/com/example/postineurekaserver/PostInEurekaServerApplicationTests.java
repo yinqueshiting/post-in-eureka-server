38d44bd518a701cca8919db9299cb8311a9bc6fb
给我红发一个面子 <yuanzihao147@foxmail.com>
2019-12-15 19:55:07 +0800

    修改打包异常

--- src/test/java/com/example/postineurekaserver/PostInEurekaServerApplicationTests.java
+++ src/test/java/com/example/postineurekaserver/PostInEurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.postineurekaserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PostInEurekaServerApplicationTests {
+public class PostInEurekaServerApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
